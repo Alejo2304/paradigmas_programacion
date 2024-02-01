47feb793b14eb813ef9678b6333623bbf29a73ea
Alejo <ojeda.alejandro.2304@gmail.com>
2024-02-01 16:00:53 -0500

    Created method to update codigo value

--- POO/day2/billing.py
+++ POO/day2/billing.py
@@ -1,7 +1,15 @@
 class Cliente: 
     def __init__(self):
-        pass
+        self.codigo = 0
+        self.nombre = ""
+        self.telefono = ""
+        self.email = ""
+        self.credito = 0.0
 
+    def actualizarCodigo(self, cod):
+        self.codigo = cod
+        
+        
 class Vendedor:
     def __init__(self):
         pass
