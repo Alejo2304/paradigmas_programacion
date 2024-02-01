c1eceec245504193e9da538c7b32f42248d85735
Alejo <ojeda.alejandro.2304@gmail.com>
2024-02-01 15:46:30 -0500

    created project for the programing paradigms for the university

---
+++ POO/day2/billing.py
@@ -0,0 +1,16 @@
+class Cliente: 
+    def __init__(self):
+        pass
+
+class Vendedor:
+    def __init__(self):
+        pass
+
+class Producto:
+    def __init__(self):
+        pass
+
+class Factura:
+    def __init__(self):
+        pass
+
